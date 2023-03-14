dc171c1ad92be3574a59eb22730058e82e6a4d47
bloatoo-git <mkawr20@gmail.com>
2023-03-14 12:54:13 +0100

    update

--- mundane/before/cfg_player_opartions.py
+++ mundane/before/cfg_player_opartions.py
@@ -9,29 +9,32 @@ PATH_PLAYERS = f"{dirname(abspath(__file__))}/../config/players/"
 
 def create_player():
     name = str(input("Enter your player name: "))
-    
-    if os.name == 'posix':
-        os.system(f"cd {PATH_PLAYERS} && touch {name}.ini")
-    else:
-        os.system(f"cd {PATH_PLAYERS} && type NUL > {name}.ini")
-    
-    cfg = ConfigParser()
-    
-    cfg['CONSTANTS'] = {
-        'player_name': name
-    }
-    
-    cfg['VARIABLES'] = { # starter values defined here
-        'health': 100,
-        'mana': 100,
-        'strength': 50
-    }
-
-    with open(f"{PATH_PLAYERS}{name}.ini", 'w') as cfg_player:
-        cfg.write(cfg_player)
-        cfg_player.flush()
+    if len(name) in range(1, 20):
+        if os.name == 'posix':
+            os.system(f"cd {PATH_PLAYERS} && touch {name}.ini")
+        else:
+            os.system(f"cd {PATH_PLAYERS} && type NUL > {name}.ini")
         
-    return name
+        cfg = ConfigParser()
+        
+        cfg['CONSTANTS'] = {
+            'player_name': name
+        }
+        
+        cfg['VARIABLES'] = { # starter values defined here
+            'health': 100,
+            'mana': 100,
+            'strength': 50
+        }
+
+        with open(f"{PATH_PLAYERS}{name}.ini", 'w') as cfg_player:
+            cfg.write(cfg_player)
+            cfg_player.flush()
+            
+        return name
+    else:
+        print("You did not provide a valid name. Please try again.")
+        sys.exit(1)
 
 
 def load_player():
@@ -44,7 +47,11 @@ def load_player():
             
         player_name_selection = int(input("\n: "))
         
-        return player_files[player_name_selection-1]
+        if player_name_selection in range(1, len(player_files)+1):
+            return player_files[player_name_selection-1]
+        else:
+            print("Unknown operation")
+            sys.exit(1)
     else:
         print("No existing player files")
         sys.exit(1) # implement better game logic
